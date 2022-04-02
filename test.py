4c0037da0da2bf3e7c455ba67ac56f57d9ff29f1
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-04-02 01:04:38 -0400

    pain and suffering

--- test.py
+++ test.py
@@ -12,19 +12,38 @@ with open('coordinates.csv', 'r') as f2:
         
         coordinateList.append(row)
 
+#Remove whitespace
 coordinateList = list(filter(None,coordinateList))
 
-############################################################################################################kill me now
-print(coordinateList)
-#print(list(reversed(coordinateList[0])))
+#Reverse all elements and convert to floats
+for i in range(len(coordinateList)):
+    coordinateList[i] = list(map(float,reversed(coordinateList[i])))
 
-point = Point(-79.41, 43.73)
+#Convert all coordinates to shapely points
+for i in range(len(coordinateList)):
+    coordinateList[i] = Point(coordinateList[i][0],coordinateList[i][1])
+
+areaCodewithNumCoord = []
+
+#initialize array values to store area code and num of locations
+for feature in js['features']:
+    areaCodewithNumCoord.append([feature['properties']['AREA_SHORT_CODE'],0])
+
 
 for feature in js['features']:
 
     polygon = shape(feature['geometry'])
-    #print(polygon)
     
-    if polygon.contains(point):
-        print(feature['properties']['AREA_SHORT_CODE'])
-    
+    for i in range(len(coordinateList)):
+        if polygon.contains(coordinateList[i]):
+            #print(feature['properties']['AREA_SHORT_CODE'])
+
+            for j in range(len(areaCodewithNumCoord)):
+                if areaCodewithNumCoord[j][0]==feature['properties']['AREA_SHORT_CODE']:
+                    areaCodewithNumCoord[j][1]=areaCodewithNumCoord[j][1]+1
+
+areaCodewithNumCoord.insert(0,['Neighbourhood_ID','Number_Starbucks'])
+
+with open('areaCodeNum.csv', 'w', newline="") as f3:
+    writer = csv.writer(f3)
+    writer.writerows(areaCodewithNumCoord)
